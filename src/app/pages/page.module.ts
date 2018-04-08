72a271d420c9b36d2ca790f8f3d6cafe17fb8716
Anh Do <dinhanhdo95@gmail.com>
2018-04-08 15:20:26 +0700

    Add product component and its UI

--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [HomeComponent],
-  exports: [HomeComponent]
+  declarations: [HomeComponent, ProductComponent],
+  exports: [HomeComponent, ProductComponent]
 })
 export class PageModule { }
