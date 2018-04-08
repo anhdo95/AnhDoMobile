d295340330897c4a28a49f0b23951455ed33cc46
Anh Do <dinhanhdo95@gmail.com>
2018-04-08 15:58:27 +0700

    Add contact component and its UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { DetailsComponent } from './pages/details/details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductComponent } from './pages/product/product.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'product', component: ProductComponent },
   { path: 'details', component: DetailsComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { DetailsComponent } from './details/details.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [HomeComponent, ProductComponent, DetailsComponent],
-  exports: [HomeComponent, ProductComponent, DetailsComponent]
+  declarations: [HomeComponent, ProductComponent, DetailsComponent, ContactComponent],
+  exports: [HomeComponent, ProductComponent, DetailsComponent, ContactComponent]
 })
 export class PageModule { }
