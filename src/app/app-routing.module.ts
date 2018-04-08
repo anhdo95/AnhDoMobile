3935869419a049483ddce7b52945f3429ea449f1
Anh Do <dinhanhdo95@gmail.com>
2018-04-08 16:14:34 +0700

    Add shopping cart component and its UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ProductComponent } from './pages/product/product.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'details', component: DetailsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'cart', component: ShoppingCartComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
--- src/app/pages/page.module.ts
+++ src/app/pages/page.module.ts
@@ -4,6 +4,7 @@ import { ProductComponent } from './product/product.component';
 import { DetailsComponent } from './details/details.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   imports: [
@@ -12,11 +13,12 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     ProductComponent,
     DetailsComponent,
-    ContactComponent, LoginComponent],
+    ContactComponent, LoginComponent, ShoppingCartComponent],
   exports: [HomeComponent,
     ProductComponent,
     DetailsComponent,
     ContactComponent,
-    LoginComponent]
+    LoginComponent,
+    ShoppingCartComponent]
 })
 export class PageModule { }
